790fe19ab50b1354e067404bff2cdddab5480f1a
lzr.virgo <lzr@smail.nju.edu.cn>
2022-05-10 21:01:49 +0800

    feat: complete sample and pdf fix: caution include onnxruntime will bring bug that eval can not be called

--- src/bsdfs/neuralbsdf.cpp
+++ src/bsdfs/neuralbsdf.cpp
@@ -4,7 +4,6 @@
 #include <mitsuba/render/bsdf.h>
 #include <mitsuba/render/scene.h>
 #include <mitsuba/core/fstream.h>
-#include <onnxruntime_cxx_api.h>
 #include <misc/MLP.h>
 #include <misc/sh_encode.h>
 #include <misc/utils.h>
@@ -47,11 +46,15 @@ public:
         }
         m_brdf = new BRDFNet(weights);
     }
+    // since our bsdfnet is trained on bsdf value multipied by cosine foreshortening factor
+    // there is no need to multiply cosine foreshortening factor
     Spectrum eval(const BSDFSamplingRecord &bRec,
                   EMeasure measure = ESolidAngle) const override
     {
-        auto wi = bRec.wi;
-        auto wo = bRec.wo;
+        if (measure != ESolidAngle || Frame::cosTheta(bRec.wi) <= 0 || Frame::cosTheta(bRec.wo) <= 0)
+            return Spectrum(0.0);
+        Vector wi = bRec.wi;
+        Vector wo = bRec.wo;
         auto whwd = SH_Encode::convert_to_rusinkiewicz_xyz(wi.x, wi.y, wi.z, wo.x, wo.y, wo.z);
         float pred[3];
         m_brdf->feedforward(whwd.xyz, pred);
@@ -60,19 +63,38 @@ public:
     }
     Spectrum sample(BSDFSamplingRecord &bRec, const Point2 &sample) const override
     {
-        Spectrum ret;
-        return ret;
+        if (Frame::cosTheta(bRec.wi) <= 0)
+            return Spectrum(0.0);
+        Vector wi = bRec.wi;
+        Vector wo = warp::squareToCosineHemisphere(sample);
+        bRec.wo = wo;
+        auto whwd = SH_Encode::convert_to_rusinkiewicz_xyz(wi.x, wi.y, wi.z, wo.x, wo.y, wo.z);
+        float pred[3];
+        m_brdf->feedforward(whwd.xyz, pred);
+        inverse_miu_law_compression(MIU, pred, 3);
+        return Spectrum(pred) / warp::squareToCosineHemispherePdf(wo);
     }
     Spectrum sample(BSDFSamplingRecord &bRec, Float &pdf,
                     const Point2 &sample) const override
     {
-        Spectrum ret;
-        return ret;
+        if (Frame::cosTheta(bRec.wi) <= 0)
+            return Spectrum(0.0);
+        Vector wi = bRec.wi;
+        Vector wo = warp::squareToCosineHemisphere(sample);
+        bRec.wo = wo;
+        auto whwd = SH_Encode::convert_to_rusinkiewicz_xyz(wi.x, wi.y, wi.z, wo.x, wo.y, wo.z);
+        float pred[3];
+        m_brdf->feedforward(whwd.xyz, pred);
+        inverse_miu_law_compression(MIU, pred, 3);
+        pdf = warp::squareToCosineHemispherePdf(wo);
+        return Spectrum(pred) / pdf;
     }
     Float pdf(const BSDFSamplingRecord &bRec,
               EMeasure measure = ESolidAngle) const override
     {
-        return 1;
+        if (measure != ESolidAngle || Frame::cosTheta(bRec.wi) <= 0 || Frame::cosTheta(bRec.wo) <= 0)
+            return 0.0f;
+        return warp::squareToCosineHemispherePdf(bRec.wo);
     }
     Spectrum getDiffuseReflectance(const Intersection &its) const override
     {
