eab41b2c8365c6177b3843f06edbfdacacdac1a5
lzr.virgo <lzr@smail.nju.edu.cn>
2022-12-03 19:13:03 +0800

    scene teaser and teaser_low_res

---
+++ experiment/teaser_lowres/teaser_lowres_output.sh
@@ -0,0 +1,14 @@
+# 生成结果图片
+mkdir -p /home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/output/1108001/
+
+/home/lzr/Projects/mitsuba/dist/mitsuba /home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/teaser.xml \
+    -D maxDepth=100 -D rrDepth=20 -D sampleCount=256 -D width=960 -D height=540 \
+    -D paramdir="1108001/teaser_lowres" \
+    -D paramstr1="0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Na_palik" \
+    -D paramstr2="0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Al" \
+    -D paramstr3="0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Hg" \
+    -D paramstr4="0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Ag" \
+    -D paramstr5="0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Ag" \
+    -D paramstrTable="0.1_0_0_1.5_1_1_1_1_0_0.1_0_0_Ag" \
+    -o /home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/output/1108001/teaser_ours.exr
+  
---
+++ experiment/teaser_lowres/teaser_lowres_params.sh
@@ -0,0 +1,49 @@
+
+
+#vase 1
+mkdir -p /home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_1/; \
+python /home/lzr/layeredBsdfModel/meta_texture_maker_batch.py --GPU=1 \
+    --model_dir=/home/lzr/layeredBsdfModel/checkpoints/1108001 \
+    --out_dir=/home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_1/ \
+    --sh_dim=81 --texture_dir=/home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/textures/vase_1 \
+    --surface_type=conductor --is_sv=1 --filename=0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Na_palik
+
+#vase 2
+mkdir -p /home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_2/; \
+python /home/lzr/layeredBsdfModel/meta_texture_maker_batch.py --GPU=1 \
+    --model_dir=/home/lzr/layeredBsdfModel/checkpoints/1108001 \
+    --out_dir=/home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_2/ \
+    --sh_dim=81 --texture_dir=/home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/textures/vase_2 \
+    --surface_type=conductor --is_sv=1 --filename=0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Al
+
+#vase 3
+mkdir -p /home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_3/; \
+python /home/lzr/layeredBsdfModel/meta_texture_maker_batch.py --GPU=1 \
+    --model_dir=/home/lzr/layeredBsdfModel/checkpoints/1108001 \
+    --out_dir=/home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_3/ \
+    --sh_dim=81 --texture_dir=/home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/textures/vase_3 \
+    --surface_type=conductor --is_sv=1 --filename=0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Hg
+
+vase 4
+mkdir -p /home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_4/; \
+python /home/lzr/layeredBsdfModel/meta_texture_maker_batch.py --GPU=0 \
+    --model_dir=/home/lzr/layeredBsdfModel/checkpoints/1108001 \
+    --out_dir=/home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_4/ \
+    --sh_dim=81 --texture_dir=/home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/textures/vase_4 \
+    --surface_type=conductor --is_sv=1 --filename=0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Ag
+
+#vase 5
+mkdir -p /home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_5/; \
+python /home/lzr/layeredBsdfModel/meta_texture_maker_batch.py --GPU=0 \
+    --model_dir=/home/lzr/layeredBsdfModel/checkpoints/1108001 \
+    --out_dir=/home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/vase_5/ \
+    --sh_dim=81 --texture_dir=/home/lzr/Projects/mitsuba/experiment/teaser_lowres/teaser_ours_lowres/textures/vase_5 \
+    --surface_type=conductor --is_sv=1 --filename=0.1_0_0_1.5_1_0_0_0_0_0.1_0_0_Ag
+
+#table top(not sv)
+mkdir -p /home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/table_top/; \
+python /home/lzr/layeredBsdfModel/meta_texture_maker_batch.py --GPU=0 \
+    --model_dir=/home/lzr/layeredBsdfModel/checkpoints/1108001 \
+    --out_dir=/home/lzr/layeredBsdfData/meta_texture/1108001/teaser_lowres/table_top/ \
+    --sh_dim=81 \
+    --surface_type=conductor --is_sv=0 --filename=0.1_0_0_1.5_1_1_1_1_0_0.1_0_0_Ag
